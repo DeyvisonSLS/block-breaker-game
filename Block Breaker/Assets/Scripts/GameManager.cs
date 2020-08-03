c49b5847e92b771b3a9062a72d0be14e43e48145
Deyvison Silas <deyvisonsilas122@gmail.com>
2020-08-03 20:57:42 -0300

    Implemented speed control of time

--- Block Breaker/Assets/Scripts/GameManager.cs
+++ Block Breaker/Assets/Scripts/GameManager.cs
@@ -6,6 +6,23 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private int _blocksInScene = 0;
+    [Range(0.0f, 10.0f)]
+    [SerializeField]
+    private float _gameSpeed = 1.0f;
+    private float fixedDeltaTime;
+    void Awake()
+    {
+        this.fixedDeltaTime = 0.02f;
+        Debug.Log("fixedDeltaTime on awake is: " + this.fixedDeltaTime);
+    }
+    void Update()
+    {
+        // Debug.Log("deltaTime: " + Time.deltaTime);
+        // Debug.Log("fixedDeltaTime: " + Time.fixedDeltaTime);
+        Time.timeScale = _gameSpeed;
+        // Time.maximumDeltaTime =  _gameSpeed;
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+    }
     public void IncreaseBlockCount()
     {
         _blocksInScene++;
